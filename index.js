95b05fec61d1e65e887094e9b0735928a8aa40bd
PAREEKCHITRANSHU <87847968+PAREEKCHITRANSHU@users.noreply.github.com>
2024-07-20 00:20:16 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,10 +16,10 @@ require("./config/config").connect();
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
-app.use(express.static(path.join(__dirname, "/public"));
+// app.use(express.static(path.join(__dirname, "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(express.static("public"));
+app.use(express.static("public"));
 //routes
 app.use("/", adminRouter);
 //app.use(router);
